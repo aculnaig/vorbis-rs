1caa145b047f5e2a4e8aa4ceed608fd74c254be1
Gianluca Cannata <gcannata23gmail.com>
2025-02-22 16:28:31 +0100

    Generate libvorbis bindings

---
+++ vorbis-sys/build.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+    // Tell cargo to tell rustc to link the system's vorbis
+    // shared library.
+    println!("cargo:rustc-link-lib=vorbis");
+    println!("cargo:rustc-link-lib=vorbisenc");
+
+    // The binding::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("vorbis_sys.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Add the include path for the vorbis library
+        .clang_arg("-I/usr/include/x86_64-linux-gnu/vorbisenc")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
--- vorbis-sys/src/lib.rs
+++ vorbis-sys/src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::ptr::null_mut;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_vorbis_encode_init() {
+
+        let mut info: vorbis_info = vorbis_info {
+            version: 0,
+            channels: 0,
+            rate: 0,
+            bitrate_upper: 0,
+            bitrate_nominal: 0,
+            bitrate_lower: 0,
+            bitrate_window: 0,
+            codec_setup: null_mut(),
+        };
+
+        unsafe {
+            vorbis_info_init(&mut info);
+        }
+
+        unsafe {
+            match vorbis_encode_init(&mut info, 44100, 2, 124_000, 62_000, 8_000) {
+                0 => println!("vorbis_encode_init success"),
+                _ => println!("vorbis_encode_init failed"),
+            }
+        }
+
+        unsafe {
+            vorbis_info_clear(&mut info);
+        }
     }
 }
